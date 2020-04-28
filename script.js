169f1e387945a2833790b1745f91db582d8e5080
Josh <joshuajmv@me.com>
2020-04-28 14:00:41 +0300

    ok

--- script.js
+++ script.js
@@ -2,40 +2,52 @@
 //////////global vars//////////
 
 var clickedAlbum;
-var musicPlayer
+var musicPlayer;
+var album;
 
 //change album vars to strings
 var afterHours = "after-hours";
-
+var melancholy = "melancholy";
+var starboy = "starboy";
+var beauty = "beauty";
+var kiss = "kiss";
+var balloons = "balloons";
+var thrusday = "thrusday";
+var echoes = "echoes";
 
 //////////functions//////////
 
 function previewMusic (album){
 	clickedAlbum = document.getElementById(String(album));
 	musicPlayer = document.getElementById("music-player");
-	//show music player
-	//change preview button from the album to close preview
 	if (musicPlayer.style.display=="flex"){
-			musicPlayer.style.display="none"
+			closeMusic();
 		}
 		else{
-			musicPlayer.style.display="flex"
+			//show music player
+			musicPlayer.style.display="flex";
+			//change preview button from the album to close preview
+			document.getElementById(album).textContent = "CLOSE PREVIEW";
 			//rename album in music player
-			var newName = document.getElementById(album+"-name").textContent
+			var newName = document.getElementById(album+"-name").textContent;
 			document.getElementById("album-name").textContent = newName;
+			// hide play button
+		  document.getElementById("play-button").style.display = "none";
+		  // show pause button
+		  document.getElementById("pause-button").style.display = "block";
 		}
-		alert(album);
 
-	// show pause button
-	// hide play button
 	// play song
 	// get name of Song in music player
 }
 
-function closeMusic(){
+function closeMusic(album){
 	//close music player
 	document.getElementById("music-player").style.display = "none";
- // stop music
+	//set stop preview button to preview
+	document.getElementById(album).textContent = "CLOSE PREVIEW";
+	// stop music
+
 }
 
 function pauseMusic(){
