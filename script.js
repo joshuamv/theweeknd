329c8c704bb322b56c16edcb0dd027954a888594
Josh <joshuajmv@me.com>
2020-04-29 18:52:27 +0300

    ok

--- script.js
+++ script.js
@@ -16,43 +16,17 @@ var thrusday = "thrusday";
 var echoes = "echoes";
 
 //define songs by album//
-
 var afterHoursPlay = 'audio/blinding-lights.mp3';
 var melancholyPlay = 'audio/blinding-lights.mp3';
 var starboyPlay = 'audio/blinding-lights.mp3';
+var beautyPlay = 'audio/blinding-lights.mp3';
+var kissPlay = 'audio/blinding-lights.mp3';
+var balloonsPlay = 'audio/blinding-lights.mp3';
+var thrusdayPlay = 'audio/blinding-lights.mp3';
+var echoesPlay = 'audio/blinding-lights.mp3';
 
 var player = new Audio();
 
-
-function playAfterHours(album){
-// get name of Song in music player
-player.currentTime = 0
-player.pause();
-document.getElementById("song-name").textContent = "Blinding Lights";
-previewMusic(album);
-player.src = afterHoursPlay;
-playMusic();
-}
-
-function playMelancholy(album){
-// get name of Song in music player
-player.currentTime = 0
-player.pause();
-document.getElementById("song-name").textContent = "Call Out My Name";
-previewMusic(album);
-player.src = melancholyPlay;
-playMusic();
-}
-
-function playStarboy(album){
-// get name of Song in music player
-player.currentTime = 0
-player.pause();
-document.getElementById("song-name").textContent = "Stayboysong";
-previewMusic(album);
-player.src = starboyPlay;
-playMusic();
-}
 //////////functions//////////
 
 function previewMusic (album){
@@ -109,4 +83,87 @@ function pauseMusic(){
  document.getElementById("play-button").style.display = "block";
  // pause music
  player.pause();
+}
+
+
+//////PLAY EACH ALBUM PREVIEW///////
+
+function playAfterHours(album){
+// get name of Song in music player
+player.currentTime = 0
+player.pause();
+document.getElementById("song-name").textContent = "Blinding Lights";
+previewMusic(album);
+player.src = afterHoursPlay;
+playMusic();
+}
+
+function playMelancholy(album){
+// get name of Song in music player
+player.currentTime = 0
+player.pause();
+document.getElementById("song-name").textContent = "Call Out My Name";
+previewMusic(album);
+player.src = melancholyPlay;
+playMusic();
+}
+
+function playStarboy(album){
+// get name of Song in music player
+player.currentTime = 0
+player.pause();
+document.getElementById("song-name").textContent = "Stayboysong";
+previewMusic(album);
+player.src = starboyPlay;
+playMusic();
+}
+
+function playBeauty(album){
+// get name of Song in music player
+player.currentTime = 0
+player.pause();
+document.getElementById("song-name").textContent = "Often";
+previewMusic(album);
+player.src = beautyPlay;
+playMusic();
+}
+
+function playKiss(album){
+// get name of Song in music player
+player.currentTime = 0
+player.pause();
+document.getElementById("song-name").textContent = "okok";
+previewMusic(album);
+player.src = kissPlay;
+playMusic();
+}
+
+function playBalloons(album){
+// get name of Song in music player
+player.currentTime = 0
+player.pause();
+document.getElementById("song-name").textContent = "ooijio";
+previewMusic(album);
+player.src = balloonsPlay;
+playMusic();
+}
+
+function playThursday(album){
+// get name of Song in music player
+player.currentTime = 0
+player.pause();
+document.getElementById("song-name").textContent = "ooijio";
+previewMusic(album);
+player.src = thrusdayPlay;
+playMusic();
+}
+
+function playEchoes(album){
+// get name of Song in music player
+player.currentTime = 0
+player.pause();
+document.getElementById("song-name").textContent = "ooijio";
+previewMusic(album);
+player.src = echoesPlay;
+playMusic();
 }
