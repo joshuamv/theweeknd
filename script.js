18de565d0144717320cb494f45fbef611dfee23a
Josh <joshuajmv@me.com>
2020-04-30 19:49:28 +0300

    ok

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function closeMusic(album){
 	var previewBtn = document.getElementsByClassName("secondary-button");
 	var i;
 	for (i = 0; i < previewBtn.length; i++) {
-		previewBtn[i].textContent = <img class="play-icon" src="img/play.svg" alt="play-icon">PREVIEW;
+		previewBtn[i].textContent = PREVIEW;
 	}
 	// stop music
 	player.currentTime = 0
